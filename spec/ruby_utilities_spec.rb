ad648a349cb7db6b7b7a6ed459ab251a13eca453
Jeffrey A. Gipson <jeffagipson@gmail.com>
2010-03-12 14:04:30 -0600

    FIXED: added missing monkey dependancy

--- spec/ruby_utilities_spec.rb
+++ spec/ruby_utilities_spec.rb
@@ -1,4 +1,5 @@
 require 'lib/R1.8-Kernel_extension' if defined?(RUBY_VERSION) and RUBY_VERSION =~ /1\.8\.*/
+require 'lib/R1.8-Array_extension' if defined?(RUBY_VERSION) and RUBY_VERSION =~ /1\.8\.*/
 require "lib/ruby_utilities.rb"
 
 describe ThreeSegmentNumericVersion, "valid initialization" do
